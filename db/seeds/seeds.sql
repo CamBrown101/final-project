884f92b80b0509add6335d9fdb174a11437c1667
marcellmueller <mlmueller@protonmail.com>
2021-01-14 14:51:28 -0800

    Created rough draft of tables.sql migrations file

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO users (firstName, lastName, email, password, is_admin, user_since)
-VALUES ('Marcel', 'Mueller', 'mail.marcelm@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',  FALSE, CLOCK_TIMESTAMP());
+INSERT INTO employee (firstName, lastName, email, pin, is_admin, start_date)
+VALUES ('Cam', 'Brown', 'cameron-brown@live.com', '1111',  TRUE, CLOCK_TIMESTAMP()),
+VALUES ('Michael', 'Krishna', 'test@test.com', '2222',  TRUE, CLOCK_TIMESTAMP()),
+VALUES ('Marcel', 'Mueller', 'mail.marcelm@gmail.com', '4444',  TRUE, CLOCK_TIMESTAMP());
 
