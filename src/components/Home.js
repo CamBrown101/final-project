ede5dfddec48b95bd0ba692e6694b2f791e35582
cambrown101 <cameron-brown@live.com>
2021-01-14 22:22:17 -0800

    Add Axio call for menu items

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../UserContext';
 import { Redirect } from 'react-router-dom';
+import MenuContainer from './MenuContainer';
+import Axios from 'axios';
 
 export default function Home() {
   const { user, logout } = useContext(UserContext);
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    Axios.get('/api/menu').then((res) => {
+      setMenu(res.data);
+    });
+  }, []);
+
   if (!user.auth) {
     return <Redirect to="/login" />;
   }
 
   return (
-    <>
+    <div>
       <h1>Hello, {user.name}!</h1>
       <button onClick={logout}>Logout</button>
-    </>
+      <MenuContainer menu={menu} />
+    </div>
   );
 }
