6b1f1f139a89493dc16a47f7602d191e3f487918
Michael <michaelkrishna@hotmail.com>
2021-01-14 17:12:17 -0800

    Employee id route

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    console.log("user route");
+    console.log("employee route");
     db.query(`SELECT * FROM employees;`)
       .then((data) => {
         const employees = data.rows;
@@ -13,5 +13,23 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+
+  router.get("/:id", (req, res) => {
+    console.log("employee id route");
+    const employee = req.params.id;
+    db.query(
+      `SELECT * FROM employees
+              WHERE id = $1;`,
+      [employee]
+    )
+      .then((data) => {
+        const employees = data.rows;
+        res.send(employees);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
   return router;
 };
