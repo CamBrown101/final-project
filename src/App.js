fa6e0bafb9d76a6c5fb1095e3a559c6ff9e3911e
cambrown101 <cameron-brown@live.com>
2021-01-14 22:21:30 -0800

    Add menu url route

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserContext } from './UserContext';
 import Login from './components/Login';
 import Home from './components/Home';
-
 import CreateEmployee from './components/manager/CreateEmployee';
+
 function App() {
   const { user } = useContext(UserContext);
   console.log(user);
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/menu" component={Home} />
         <Route path="/" component={Home} />
       </Switch>
       <CreateEmployee></CreateEmployee>
