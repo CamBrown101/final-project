fa4513d46f73b96a85f786ddccfaa2d931eb7aad
cambrown101 <cameron-brown@live.com>
2021-01-14 19:21:08 -0800

    Add useContext for login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.scss';
+import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserContext } from './UserContext';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
+  const { user } = useContext(UserContext);
+  console.log(user);
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Login} />
         <Route path="/login" component={Login} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
