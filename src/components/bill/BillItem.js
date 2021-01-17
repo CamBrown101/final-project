7af682817a18c827f0e5ab6786d87da7398c129a
cambrown101 <cameron-brown@live.com>
2021-01-16 16:26:16 -0800

    Add billItem select state

--- src/components/bill/BillItem.js
+++ src/components/bill/BillItem.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BillItem.scss';
 
 export default function BillItem({ name, price }) {
+  const [selected, setSelected] = useState(false);
   return (
-    <li className="bill-item">
+    <li
+      className={`bill-item ${selected ? 'selected' : ''}`}
+      onClick={selected ? () => setSelected(false) : () => setSelected(true)}>
       <p className="item-name">{name}</p>
       <p className="item-price">${price.toFixed(2)}</p>
     </li>
