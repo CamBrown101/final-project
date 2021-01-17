9b47320c695cdc3d207f34f9ca0e5dcc3e6f9f51
cambrown101 <cameron-brown@live.com>
2021-01-17 14:50:33 -0800

    Seperate print button functionality

---
+++ src/components/bill/PrintBillButton.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+export default function PrintBillButton({ email, setEmail, printBill }) {
+  const [printToggle, setPrintToggle] = useState('hide');
+
+  return (
+    <div className="print-section">
+      <div
+        className="print-button button"
+        onClick={() => {
+          printToggle === 'hide'
+            ? setPrintToggle('show')
+            : setPrintToggle('hide');
+        }}>
+        Print
+      </div>
+      <input
+        value={email}
+        className={printToggle + ' edit-input'}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}></input>
+      <div className="confirm-cancel-buttons">
+        <div
+          className={printToggle + ' button send-button'}
+          onClick={() => {
+            printBill();
+            setEmail('');
+            setPrintToggle('hide');
+          }}>
+          Confrim
+        </div>
+        <div
+          className={printToggle + ' button cancel-button'}
+          onClick={() => {
+            setEmail('');
+            setPrintToggle('hide');
+          }}>
+          Cancel
+        </div>
+      </div>
+    </div>
+  );
+}
