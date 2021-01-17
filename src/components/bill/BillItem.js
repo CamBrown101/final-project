674763d9f735fb6a13a8443dddbe07cc9dcd58fa
cambrown101 <cameron-brown@live.com>
2021-01-16 16:41:14 -0800

    Add mod html

--- src/components/bill/BillItem.js
+++ src/components/bill/BillItem.js
@@ -3,12 +3,16 @@ import './BillItem.scss';
 
 export default function BillItem({ name, price }) {
   const [selected, setSelected] = useState(false);
+  const mod = false;
   return (
     <li
       className={`bill-item ${selected ? 'selected' : ''}`}
       onClick={selected ? () => setSelected(false) : () => setSelected(true)}>
-      <p className="item-name">{name}</p>
-      <p className="item-price">${price.toFixed(2)}</p>
+      <div className="item">
+        <p className="item-name">{name}</p>
+        <p className="item-price">${price.toFixed(2)}</p>
+      </div>
+      {mod ? <p className="mod">MOD</p> : <></>}
     </li>
   );
 }
