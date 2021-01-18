3e7415410248313d3f774b4e8f570072b7dafc1f
cambrown101 <cameron-brown@live.com>
2021-01-18 14:36:00 -0800

    Add ability to selct order items

--- src/components/productionScreen/ProductionContainer.js
+++ src/components/productionScreen/ProductionContainer.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-import React from "react";
-import "./ProductionContainer.scss";
-import "./ProductionOrder";
-import ProductionOrder from "./ProductionOrder";
-import Axios from "axios";
+import axios from 'axios';
+import React from 'react';
+import './ProductionContainer.scss';
+import './ProductionOrder';
+import ProductionOrder from './ProductionOrder';
+import Axios from 'axios';
 
 export default function ProductionContainer(props) {
   Axios.get(`/api/orders/production`).then((orders) => {
@@ -12,7 +12,11 @@ export default function ProductionContainer(props) {
   return (
     <div className="prod-container">
       <h1>Production SCreen</h1>
-      <ProductionOrder items={props.items} />
+      <div className="prod-order-container">
+        <ProductionOrder items={props.items} id="1" />
+        <ProductionOrder items={props.items} id="2" />
+        <ProductionOrder items={props.items} id="3" />
+      </div>
     </div>
   );
 }
--- src/components/productionScreen/ProductionOrder.js
+++ src/components/productionScreen/ProductionOrder.js
@@ -1,17 +1,38 @@
-import React from "react";
-import "./ProductionOrder.scss";
-import "./ProductionOrderItem";
-import ProductionOrderItem from "./ProductionOrderItem";
+import React, { useState } from 'react';
+import './ProductionOrder.scss';
+import './ProductionOrderItem';
+import ProductionOrderItem from './ProductionOrderItem';
 
 export default function ProductionOrder(props) {
   // const orders = props.items.map((item) => {
   //   return <ProductionOrderItem />
   // })
+  const [selectedItems, setSelectedItems] = useState([]);
   return (
     <div className="prod-order">
       <h3> ORDER NUMBER</h3>
       orders
       <br />
+      <ProductionOrderItem
+        id="1"
+        selectedItems={selectedItems}
+        setSelectedItems={setSelectedItems}
+      />
+      <ProductionOrderItem
+        id="2"
+        selectedItems={selectedItems}
+        setSelectedItems={setSelectedItems}
+      />
+      <ProductionOrderItem
+        id="3"
+        selectedItems={selectedItems}
+        setSelectedItems={setSelectedItems}
+      />
+      <ProductionOrderItem
+        id="4"
+        selectedItems={selectedItems}
+        setSelectedItems={setSelectedItems}
+      />
     </div>
   );
 }
