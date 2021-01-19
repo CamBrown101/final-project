932a539055d64cc9ff6b72c7173f8919e8ffb0b5
cambrown101 <cameron-brown@live.com>
2021-01-18 19:29:52 -0800

    Clean up dropdown html

--- src/components/bill/EditButton.js
+++ src/components/bill/EditButton.js
@@ -21,30 +21,32 @@ export default function EditButton({ mod, setMod, bill, selected, data }) {
         }}>
         Edit
       </div>
-      <input
-        value={mod}
-        className={inputToggle + ' edit-input'}
-        onChange={(event) => {
-          setMod(event.target.value);
-        }}></input>
-      <div className="confirm-cancel-buttons">
-        <div
-          className={inputToggle + ' button send-button'}
-          onClick={() => {
-            if (selected < bill.items.length)
-              bill.items[bill.items.length - 1 - selected].mods = mod;
-            setMod('');
-            setInputToggle('hide');
-          }}>
-          Confrim
-        </div>
-        <div
-          className={inputToggle + ' button cancel-button'}
-          onClick={() => {
-            setMod('');
-            setInputToggle('hide');
-          }}>
-          Cancel
+      <div className={inputToggle + ' dropdown'}>
+        <input
+          value={mod}
+          className="edit-input"
+          onChange={(event) => {
+            setMod(event.target.value);
+          }}></input>
+        <div className="confirm-cancel-buttons">
+          <div
+            className={'button send-button'}
+            onClick={() => {
+              if (selected < bill.items.length)
+                bill.items[bill.items.length - 1 - selected].mods = mod;
+              setMod('');
+              setInputToggle('hide');
+            }}>
+            Confrim
+          </div>
+          <div
+            className={'button cancel-button'}
+            onClick={() => {
+              setMod('');
+              setInputToggle('hide');
+            }}>
+            Cancel
+          </div>
         </div>
       </div>
     </div>
