a879351bac9c4a3122059f25b3701ad7403ab737
Michael <michaelkrishna@hotmail.com>
2021-01-20 11:12:19 -0800

    Added routes for sales and gross profit

---
+++ src/routes/analytics.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  //gets all shifts
+  router.get("/labour", (req, res) => {
+    console.log("shifts route");
+    db.query(`SELECT * FROM shifts;`)
+      .then((data) => {
+        const shifts = data.rows;
+        res.send(shifts);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.get("/sales", (req, res) => {
+    console.log("shifts route");
+    db.query(
+      `
+      SELECT price, timestamp from order_items
+      JOIN menu_items ON order_items.item= menu_items.id
+      WHERE timestamp > now() - interval '1 week';`
+    )
+      .then((data) => {
+        const shifts = data.rows;
+        res.send(shifts);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.get("/gross-sales", (req, res) => {
+    console.log("shifts route");
+    db.query(
+      `
+      SELECT sum(price) from order_items
+      JOIN menu_items ON order_items.item= menu_items.id
+      WHERE timestamp > now() - interval '1 week';`
+    )
+      .then((data) => {
+        const shifts = data.rows;
+        res.send(shifts);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
