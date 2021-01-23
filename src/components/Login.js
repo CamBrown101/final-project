9a714b145c4c51ea9d5997f621d6641a9c22e96f
Michael <michaelkrishna@hotmail.com>
2021-01-23 15:53:10 -0800

    Navbar color changed to match rest of style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,12 @@
-import './Login.scss';
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../UserContext';
-import { Redirect } from 'react-router-dom';
+import "./Login.scss";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../UserContext";
+import { Redirect } from "react-router-dom";
 
 export default function Login() {
   const { user, login } = useContext(UserContext);
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
 
   if (user.auth) {
     return <Redirect to="/menu" />;
@@ -21,84 +21,97 @@ export default function Login() {
   return (
     <div className="login-container">
       <h1>Sign In</h1>
-      <p className="input">{code}</p>
-      <div className="button-container">
-        <button
-          onClick={() => {
-            addNumber('1');
-          }}>
-          1
-        </button>
-        <button
-          onClick={() => {
-            addNumber('2');
-          }}>
-          2
-        </button>
-        <button
-          onClick={() => {
-            addNumber('3');
-          }}>
-          3
-        </button>
-        <br />
-        <button
-          onClick={() => {
-            addNumber('4');
-          }}>
-          4
-        </button>
-        <button
-          onClick={() => {
-            addNumber('5');
-          }}>
-          5
-        </button>
-        <button
-          onClick={() => {
-            addNumber('6');
-          }}>
-          6
-        </button>
-        <br />
+      <div className="keypad-container">
+        <p className="input">{code}</p>
+        <div className="button-container">
+          <button
+            onClick={() => {
+              addNumber("1");
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              addNumber("2");
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              addNumber("3");
+            }}
+          >
+            3
+          </button>
+          <br />
+          <button
+            onClick={() => {
+              addNumber("4");
+            }}
+          >
+            4
+          </button>
+          <button
+            onClick={() => {
+              addNumber("5");
+            }}
+          >
+            5
+          </button>
+          <button
+            onClick={() => {
+              addNumber("6");
+            }}
+          >
+            6
+          </button>
+          <br />
 
-        <button
-          onClick={() => {
-            addNumber('7');
-          }}>
-          7
-        </button>
-        <button
-          onClick={() => {
-            addNumber('8');
-          }}>
-          8
-        </button>
-        <button
-          onClick={() => {
-            addNumber('9');
-          }}>
-          9
-        </button>
-        <br />
+          <button
+            onClick={() => {
+              addNumber("7");
+            }}
+          >
+            7
+          </button>
+          <button
+            onClick={() => {
+              addNumber("8");
+            }}
+          >
+            8
+          </button>
+          <button
+            onClick={() => {
+              addNumber("9");
+            }}
+          >
+            9
+          </button>
+          <br />
 
-        <button onClick={() => login(code)}>Submit</button>
-        <button
-          onClick={() => {
-            addNumber('0');
-          }}>
-          0
-        </button>
-        <button
-          onClick={() => {
-            setCode('');
-          }}>
-          Clear
-        </button>
+          <button onClick={() => login(code)}>Submit</button>
+          <button
+            onClick={() => {
+              addNumber("0");
+            }}
+          >
+            0
+          </button>
+          <button
+            onClick={() => {
+              setCode("");
+            }}
+          >
+            Clear
+          </button>
+        </div>
+        <Link to={"/timecard"}>
+          <button>Clock In</button>
+        </Link>
       </div>
-      <Link to={'/timecard'}>
-        <button>Clock In</button>
-      </Link>
     </div>
   );
 }
