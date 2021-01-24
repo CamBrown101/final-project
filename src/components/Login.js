a015d2cfda2795cb7043eb1d07911eeea179c429
cambrown101 <cameron-brown@live.com>
2021-01-24 12:18:15 -0800

    Fix styling on login and punchin screens

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,12 @@
-import "./Login.scss";
-import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import { UserContext } from "../UserContext";
-import { Redirect } from "react-router-dom";
+import './Login.scss';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../UserContext';
+import { Redirect } from 'react-router-dom';
 
 export default function Login() {
   const { user, login } = useContext(UserContext);
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState('');
 
   if (user.auth) {
     return <Redirect to="/menu" />;
@@ -20,74 +20,65 @@ export default function Login() {
 
   return (
     <div className="login-container">
-      <h1>Sign In</h1>
+      <h1 className="login-title">Sign In</h1>
       <div className="keypad-container">
         <p className="input">{code}</p>
         <div className="button-container">
           <button
             onClick={() => {
-              addNumber("1");
-            }}
-          >
+              addNumber('1');
+            }}>
             1
           </button>
           <button
             onClick={() => {
-              addNumber("2");
-            }}
-          >
+              addNumber('2');
+            }}>
             2
           </button>
           <button
             onClick={() => {
-              addNumber("3");
-            }}
-          >
+              addNumber('3');
+            }}>
             3
           </button>
           <br />
           <button
             onClick={() => {
-              addNumber("4");
-            }}
-          >
+              addNumber('4');
+            }}>
             4
           </button>
           <button
             onClick={() => {
-              addNumber("5");
-            }}
-          >
+              addNumber('5');
+            }}>
             5
           </button>
           <button
             onClick={() => {
-              addNumber("6");
-            }}
-          >
+              addNumber('6');
+            }}>
             6
           </button>
           <br />
 
           <button
             onClick={() => {
-              addNumber("7");
-            }}
-          >
+              addNumber('7');
+            }}>
             7
           </button>
           <button
             onClick={() => {
-              addNumber("8");
-            }}
-          >
+              addNumber('8');
+            }}>
             8
           </button>
           <button
             onClick={() => {
-              addNumber("9");
-            }}
-          >
+              addNumber('9');
+            }}>
             9
           </button>
           <br />
@@ -95,22 +86,22 @@ export default function Login() {
           <button onClick={() => login(code)}>Submit</button>
           <button
             onClick={() => {
-              addNumber("0");
-            }}
-          >
+              addNumber('0');
+            }}>
             0
           </button>
           <button
             onClick={() => {
-              setCode("");
-            }}
-          >
+              setCode('');
+            }}>
             Clear
           </button>
         </div>
-        <Link to={"/timecard"}>
-          <button>Clock In</button>
-        </Link>
+        <div className="clockin-button-container">
+          <Link to={'/timecard'}>
+            <button className="clockin-button">Clock In</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
