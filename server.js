0c5d149461fba30594eaae5443497dedadce792f
cambrown101 <cameron-brown@live.com>
2021-01-25 14:38:55 -0800

    Remove heroku code causing crash

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
-require("dotenv").config();
+require('dotenv').config();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const { Client } = require("pg");
+const { Client } = require('pg');
 const db = new Client({
   connectionString: process.env.DATABASE_URL,
 });
@@ -19,36 +19,30 @@ db.connect();
 const PORT = process.env.PORT || 8080;
 
 //import routes
-const loginRoutes = require("./src/routes/login");
-const employeeRoutes = require("./src/routes/employees");
-const menuRoutes = require("./src/routes/menu");
-const orderRoutes = require("./src/routes/orders");
-const tableRoutes = require("./src/routes/tables");
-const categoryRoutes = require("./src/routes/categories");
-const timecardRoutes = require("./src/routes/timecards");
-const analyticRoutes = require("./src/routes/analytics");
-const paymentRoutes = require("./src/routes/payments");
+const loginRoutes = require('./src/routes/login');
+const employeeRoutes = require('./src/routes/employees');
+const menuRoutes = require('./src/routes/menu');
+const orderRoutes = require('./src/routes/orders');
+const tableRoutes = require('./src/routes/tables');
+const categoryRoutes = require('./src/routes/categories');
+const timecardRoutes = require('./src/routes/timecards');
+const analyticRoutes = require('./src/routes/analytics');
+const paymentRoutes = require('./src/routes/payments');
 
 app.use(cors());
-if (process.env.NODE_ENV === "production"{
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
-}
 
 //use routes
-app.use("/login", loginRoutes(db));
-app.use("/api/employees", employeeRoutes(db));
-app.use("/api/menu", menuRoutes(db));
-app.use("/api/orders", orderRoutes(db));
-app.use("/api/tables", tableRoutes(db));
-app.use("/api/categories", categoryRoutes(db));
-app.use("/api/timecards", timecardRoutes(db));
-app.use("/api/analytics", analyticRoutes(db));
-app.use("/api/payments", paymentRoutes(db));
+app.use('/login', loginRoutes(db));
+app.use('/api/employees', employeeRoutes(db));
+app.use('/api/menu', menuRoutes(db));
+app.use('/api/orders', orderRoutes(db));
+app.use('/api/tables', tableRoutes(db));
+app.use('/api/categories', categoryRoutes(db));
+app.use('/api/timecards', timecardRoutes(db));
+app.use('/api/analytics', analyticRoutes(db));
+app.use('/api/payments', paymentRoutes(db));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   console.log(req.session);
 });
 
