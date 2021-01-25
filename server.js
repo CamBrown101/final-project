09001e2b16fea5be1278b36ca8ba51610af3f18a
Michael <michaelkrishna@hotmail.com>
2021-01-25 13:42:27 -0800

    Fix to deployment

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ const analyticRoutes = require("./src/routes/analytics");
 const paymentRoutes = require("./src/routes/payments");
 
 app.use(cors());
+if (process.env.NODE_ENV === "production"{
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
+
 //use routes
 app.use("/login", loginRoutes(db));
 app.use("/api/employees", employeeRoutes(db));
