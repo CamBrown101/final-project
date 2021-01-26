ec179866597c38d66c694cf1a799838fe547998b
cambrown101 <cameron-brown@live.com>
2021-01-25 19:58:16 -0800

    Fix Axios

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const API_URL = process.env.API_URL;
+console.log(API_URL);
 const api = axios.create({
   baseURL: API_URL, // or process.env.BASE_URL if not using CRA
 });
